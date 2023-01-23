1ec41699889a77672c56deeddd66d62069d03ad4
MaqhobelaKao <kaomaqhobela@gmail.com>
2023-01-24 01:08:45 +0200

    Initial project seed structure

---
+++ src/main/java/com/liparola/umang/UmangApplication.java
@@ -0,0 +1,12 @@
+package com.liparola.umang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmangApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UmangApplication.class, args);
+    }
+
+}
